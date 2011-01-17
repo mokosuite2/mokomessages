f31dde39325f552a57e93382dcb58d5c684ef6b2
Daniele Ricci <daniele.athome@gmail.com>
2011-01-17 16:42:33 +0100

    QVGA prepare
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- run-messages.sh
+++ run-messages.sh
@@ -7,4 +7,4 @@ fi
 
 make &&
 cd data && sudo make install && cd .. &&
-DISPLAY=$DISPLAY ELM_FINGER_SIZE=70 ELM_SCALE=2 ELM_THEME=gry DBUS_SYSTEM_BUS_ADDRESS="tcp:host=neo,port=8000" src/mokomessages
+DISPLAY=$DISPLAY ELM_FINGER_SIZE=35 ELM_SCALE=1 ELM_THEME=gry DBUS_SYSTEM_BUS_ADDRESS="tcp:host=neo,port=8000" src/mokomessages
